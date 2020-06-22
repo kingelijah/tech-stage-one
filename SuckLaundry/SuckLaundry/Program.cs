5a85dc922bf3bed7c30e352437be78d8a2200457
cidospark <cidospark27@gmail.com>
2020-06-22 20:14:05 +0100

    Inintial commit

---
+++ SuckLaundry/SuckLaundry/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SuckLaundry
+{
+    class Program
+    {
+        //Do not delete or edit this method, you can only modify the block
+        public int GetMaximumPairOfSocks(int noOfWashes, int[] cleanPile, int[] dirtyPile)
+        {
+            //You can edit from here down
+            return 0;
+        }
+
+        /**
+         * You can create various helper methods
+         * */
+    }
+}
